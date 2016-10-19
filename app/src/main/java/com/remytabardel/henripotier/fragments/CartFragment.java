ece09e046b0e3a5dbc9c1b59d321739a0f4e6522
RemyTabardel <remy.tabardel@ixxi.biz>
2016-10-19 09:20:23 +0200

    add keystore and change shape

--- app/src/main/java/com/remytabardel/henripotier/fragments/CartFragment.java
+++ app/src/main/java/com/remytabardel/henripotier/fragments/CartFragment.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.remytabardel.henripotier.MyApplication;
 import com.remytabardel.henripotier.R;
@@ -69,7 +70,7 @@ public class CartFragment extends AbstractFragment implements CartAdapterListene
     }
 
     @OnClick(R.id.button_look_collection)
-    public void onClickButtonLookCollection()
+    public void onClickButtonLookCollection(Button button)
     {
         //we simulate click on books menu
         ((MainActivity) getActivity()).performNavigationClick(R.id.nav_books);
